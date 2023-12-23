c0813f0d18c75f65d87837ea6c878d915936d135
Sangram <sangram@techmachinerylabs.ai>
2023-12-23 23:32:40 +0530

    add middleware and added function to handle error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = await bcryptjs.hash(password, 10); //random char of hard combined and create hashed password
   const newUser = new User({ username, email, password: hashedPassword });
@@ -8,6 +8,6 @@ export const signup = async (req, res) => {
     await newUser.save(); //save the data in database
     res.status(201).json({ message: "User created successfully" });
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    next(err);
   }
 };
--- api/index.js
+++ api/index.js
@@ -19,3 +19,15 @@ app.listen(3000, () => console.log("server started on port 3000 !"));
 // });
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+// Error Handling
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
