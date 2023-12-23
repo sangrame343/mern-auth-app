a0ef45aebb1934c27e910356dacb021289a5fc93
Sangram <sangram@techmachinerylabs.ai>
2023-12-23 22:12:40 +0530

    mongodb connected and env file create

--- api/index.js
+++ api/index.js
@@ -1,5 +1,12 @@
 import Express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+//password:sangramr343,databasename:mern-auth
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("connected to db"))
+  .catch((err) => console.log(err));
 const app = Express();
 
 app.listen(3000, () => console.log("server started on port 3000 !"));
