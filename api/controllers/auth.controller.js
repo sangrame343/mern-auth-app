0e792cb7648e1f90818952e69a66b8e6ad1e8db2
Sangram <sangram@techmachinerylabs.ai>
2023-12-26 14:30:26 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = await bcryptjs.hash(password, 10); //random char of hard combined and create hashed password
@@ -7,6 +9,29 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save(); //save the data in database
     res.status(201).json({ message: "User created successfully" });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validateUser = await User.findOne({ email });
+    if (!validateUser) return next(errorHandler(404, "User not found!"));
+    const validPassword = await bcryptjs.compare(
+      password,
+      validateUser.password
+    );
+    if (!validPassword) return next(errorHandler(400, "Wrong Password"));
+    const token = jwt.sign({ id: validateUser._id }, process.env.JWT_SECRET);
+    // res.json({ token, id: validateUser._id });
+    const { password: hashedPassword, ...rest } = validateUser._doc;
+    const expiryDate = new Date(Date.now() + 3600000);
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(validateUser);
   } catch (err) {
     next(err);
   }
