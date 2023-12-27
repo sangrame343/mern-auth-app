9a80b110b0db9f0af5c04249f7c9484b0fd919a0
Sangram <sangram@techmachinerylabs.ai>
2023-12-27 13:22:45 +0530

    complete upload image functionality

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,18 +1,89 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 export default function Profile() {
+  const fileRef = useRef(null);
+  const [image, setImage] = useState(undefined);
+  const [imagePersent, setImagePersent] = useState(0);
+  const [imageError, setImageError] = useState(null);
+  const [formData, setFormData] = useState({});
+  //console.log(formData);
+  //console.log(imagePersent);
+  //console.log(image);
   const { currentUser } = useSelector((state) => state.user);
+  useEffect(() => {
+    if (image) {
+      handleFileUpload(image);
+    }
+  }, [image]);
+  const handleFileUpload = async (image) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + image.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+    uploadTask.on("state_changed", (snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      setImagePersent(Math.round(progress));
+      // console.log("Upload is " + progress + "% done");
+      (error) => {
+        setImageError(true);
+      };
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setFormData({ ...formData, profilePicture: downloadURL });
+        });
+      };
+      switch (snapshot.state) {
+        case "paused":
+          console.log("Upload is paused");
+          break;
+        case "running":
+          console.log("Upload is running");
+          break;
+        default:
+          break;
+      }
+    });
+  };
   return (
     <div className="max-w-lg p-3 mx-auto">
       <h1 className="text-3xl text-black font-semibold text-center my-7 ">
         Profile
       </h1>
       <form className="flex justify-center flex-col gap-4" action="">
+        <input
+          type="file"
+          onChange={(e) => setImage(e.target.files[0])}
+          name=""
+          ref={fileRef}
+          hidden
+          accept="image/*"
+        />
         <img
+          onClick={() => fileRef.current.click()}
           src={currentUser.profilePicture}
           alt=""
           className="h-24 w-24 mt-2 self-center cursor-pointer rounded-full object-cover"
         />
+        <p className="text-sm self-center">
+          {imageError ? (
+            <span className="text-red-700">
+              Error uploading image (file size must be less than 2 MB)
+            </span>
+          ) : imagePersent > 0 && imagePersent < 100 ? (
+            <span className="text-slate-700">{`Uploading: ${imagePersent} %`}</span>
+          ) : imagePersent === 100 ? (
+            <span className="text-green-700">Image uploaded successfully</span>
+          ) : (
+            ""
+          )}
+        </p>
         <input
           defaultValue={currentUser.username}
           type="text"
